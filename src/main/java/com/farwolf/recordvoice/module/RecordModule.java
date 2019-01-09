3b66d99a1fac06f9a13e9b1fb738a0c3df9bc134
郑江荣 <362675035@qq.com>
2019-01-09 22:34:34 +0800

    first commit

---
+++ src/main/java/com/farwolf/recordvoice/module/RecordModule.java
@@ -0,0 +1,76 @@
+package com.farwolf.recordvoice.module;
+
+import android.Manifest;
+import android.app.Activity;
+
+import com.farwolf.perssion.Perssion;
+import com.farwolf.perssion.PerssionCallback;
+import com.farwolf.recordvoice.ModuleResultListener;
+import com.farwolf.recordvoice.RecorderManager;
+import com.farwolf.weex.annotation.WeexModule;
+import com.farwolf.weex.base.WXModuleBase;
+import com.taobao.weex.annotation.JSMethod;
+import com.taobao.weex.bridge.JSCallback;
+
+import java.util.HashMap;
+
+@WeexModule(name="record")
+public class RecordModule extends WXModuleBase {
+
+
+    @JSMethod
+    public void start(final HashMap param){
+//        Manifest.permission.RECORD_AUDIO
+
+
+        Perssion.check((Activity) mWXSDKInstance.getContext(), Manifest.permission.RECORD_AUDIO,new PerssionCallback(){
+
+
+            @Override
+            public void onGranted() {
+
+
+                Perssion.check((Activity) mWXSDKInstance.getContext(),Manifest.permission.WRITE_EXTERNAL_STORAGE , new PerssionCallback() {
+                    @Override
+                    public void onGranted() {
+
+                        RecorderManager.getInstance().start(param, new ModuleResultListener() {
+                            @Override
+                            public void onResult(Object o) {
+
+                            }
+                        });
+                    }
+                });
+
+
+
+            }
+        });
+
+
+    }
+
+    @JSMethod
+    public void pause(){
+        RecorderManager.getInstance().pause(new ModuleResultListener() {
+            @Override
+            public void onResult(Object o) {
+
+            }
+        });
+    }
+
+
+
+    @JSMethod
+    public void stop(final JSCallback callback){
+        RecorderManager.getInstance().stop(new ModuleResultListener() {
+            @Override
+            public void onResult(Object o) {
+                callback.invoke(o);
+            }
+        });
+    }
+
+}
